6090f5903f6caadaf88e9e801f4d7c4f84676508
stnayem <stnayem@gmail.com>
2024-07-05 10:50:03 +0800

    Login route updated

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar />
+            <h2 className="text-2xl">Login page</h2>
+        </div>
+    );
+};
+
+export default Login;
